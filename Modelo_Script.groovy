3729e9ebfc29b14db5620fc52ecfd610adfdee92
Leonardo Ledo <leonardoledo169@gmail.com>
2025-10-10 13:20:37 -0300

    Desenvolvido Scritp SCF0150P2.groovy

---
+++ Modelo_Script.groovy
@@ -0,0 +1,9 @@
+package scripts
+
+import multitec.swing.core.MultitecRootPanel;
+
+public class Modelo_Script extends sam.swing.ScriptBase{
+    @Override
+    public void execute(MultitecRootPanel tarefa) {
+    }
+}
