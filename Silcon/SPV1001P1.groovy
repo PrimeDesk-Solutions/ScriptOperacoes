e5c8a819de7f87ef43589f449addd840a37eb9a6
PrimeDesk-Solutions <leonardosuporte2@outlook.com>
2026-02-02 14:29:48 -0300

    Alterado caminho da imagem no  script SPV1001P1.groovy

--- Silcon/SPV1001P1.groovy
+++ Silcon/SPV1001P1.groovy
@@ -112,7 +112,7 @@ public class Script extends sam.swing.ScriptBase{
 
     private void exibirTelaDeAtencaoComMensagemEntidade(String msg){
 
-        String caminhoImagem = "H:/Server/homes/Sam4/imagens/atencao.png";
+        String caminhoImagem = "H:/Sam4/imagens/atencao.png";
 
         // Caixa de Mensagem
         JDialog dialog = new JDialog((Frame) null, "Mensagem de Alerta", true);
